fda37a7183da2b2ba29a96ae875411ea2ff130ed
unknown <josieoyeyipo@gmail.com>
2024-02-14 09:11:35 +0100

    added a new scroll button, and adjusted send and about section on mobile views

---
+++ assests/js/scroll.js
@@ -0,0 +1,17 @@
+window.addEventListener('scroll', function() {
+    var scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+    var scrollToTopButton = document.getElementById('scrollToTop');
+
+    if (scrollPosition > 500) {
+        scrollToTopButton.classList.add('show');
+    } else {
+        scrollToTopButton.classList.remove('show');
+    }
+});
+
+document.getElementById('scrollToTop').addEventListener('click', function() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
